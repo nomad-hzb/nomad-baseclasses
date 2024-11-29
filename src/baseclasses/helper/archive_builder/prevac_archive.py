870a3765788abb5eaf4751628c80efd5c67e57b5
Micha <michael.goette@posteo.de>
2024-11-29 14:55:52 +0100

    fixed observables

--- src/baseclasses/helper/archive_builder/prevac_archive.py
+++ src/baseclasses/helper/archive_builder/prevac_archive.py
@@ -20,11 +20,13 @@ def get_observables(observables_df, num_targets):
         temperature = step.get('Temperature  °C')
         # we set room temperature to 25 based on the obolibrary link provided in the schema
         observables.temperature = 25 if temperature == 'RT' else temperature
-        observables.bias_voltage = step.iloc[4:4+num_targets].tolist()
-        observables.bias_current = step.iloc[13:13+num_targets].tolist()
-        observables.notes = step.get('Notes') if not pd.isna(step.get('Notes')) else None
+        observables.bias_voltage = step.iloc[3:3+num_targets].tolist()
+        observables.bias_current = step.iloc[12:12+num_targets].tolist()
+        observables.notes = step.get('Notes') if not pd.isna(
+            step.get('Notes')) else None
         process_observables.append(observables)
     return process_observables
+
 
 def get_process_properties(parameters_df, num_targets):
     process_properties = []
@@ -39,16 +41,18 @@ def get_process_properties(parameters_df, num_targets):
         properties.substrate_temperature = step.get('Substrate Temperature (°C)')
         properties.ramp = step.get('Ramp (°C/min)')
         time_obj = step.get('Time (hh:mm:ss)')
-        total_seconds = time_obj.hour * 3600 + time_obj.minute * 60 + time_obj.second if not pd.isna(time_obj) else None
+        total_seconds = time_obj.hour * 3600 + time_obj.minute * \
+            60 + time_obj.second if not pd.isna(time_obj) else None
         properties.deposition_time = total_seconds
         properties.power = step.iloc[9:9+num_targets].tolist()
         properties.rotation_rate = step.get('Rotation (°/s)')
         properties.z_position = step.get('Z position')
         properties.flow_rate = step.get('Flow Rate (ml/min)')
         gas = step.get('Gas')
-        properties.gas = PubChemPureSubstanceSectionCustom(name=gas, load_data=False) if not pd.isna(gas) else None
+        properties.gas = PubChemPureSubstanceSectionCustom(
+            name=gas, load_data=False) if not pd.isna(gas) else None
         process_properties.append(properties)
-    return  process_properties
+    return process_properties
 
 
 def get_target_properties(source_configuration_df):
@@ -59,12 +63,11 @@ def get_target_properties(source_configuration_df):
         properties = TargetProperties()
         material_name = source_configuration_df.loc[position_idx, 'Source']
         properties.name = f'{position_idx + 1}) {material_name}'
-        properties.material = PubChemPureSubstanceSectionCustom(name=material_name, load_data=False)
+        properties.material = PubChemPureSubstanceSectionCustom(
+            name=material_name, load_data=False)
         properties.position = source_configuration_df.loc[position_idx, 'Position']
         properties.angle = source_configuration_df.loc[position_idx, 'Position (grad)']
         properties.rf_dc = source_configuration_df.loc[position_idx, 'RF/DC']
         target_properties.append(properties)
 
     return target_properties
-
-
