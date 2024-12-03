dd2f68bb77625c648e50e9caf477e1aeeb35c31d
carla-terboven <carla.terboven@helmholtz-berlin.de>
2024-12-03 17:07:33 +0100

    Add labview metadata

--- src/baseclasses/chemical_energy/electrolyser_performance.py
+++ src/baseclasses/chemical_energy/electrolyser_performance.py
@@ -17,97 +17,143 @@
 #
 
 import numpy as np
-
-from nomad.metainfo import (Quantity, Datetime)
-from nomad.datamodel.data import ArchiveSection
-from nomad.datamodel.metainfo.basesections import CompositeSystemReference
-from nomad.datamodel.metainfo.plot import PlotSection, PlotlyFigure
 import plotly.graph_objects as go
+from nomad.datamodel.metainfo.basesections import CompositeSystem
+from nomad.datamodel.metainfo.plot import PlotlyFigure, PlotSection
+from nomad.metainfo import Datetime, Quantity, SubSection
+
+from baseclasses import PubChemPureSubstanceSectionCustom
 
 from .. import BaseMeasurement, ReadableIdentifiersCustom
-from .cesample import build_initial_id, create_id
 
 
-#TODO imports cleaning and maybe create quantities for ids if needed
+# TODO create quantities for ids if needed
+# TODO decide which concepts should use reference instead of subsection
+class NESDElectrode(CompositeSystem):
+    electrolyte = Quantity(
+        type=str,
+        shape=[],
+    )
+
+    catalyst = Quantity(
+        type=str,
+        shape=[],
+    )
+    # TODO use a material class here or rename and reuse the materialslist from cesample??
+
+    electrode_area = Quantity(type=np.dtype(np.float64), unit='mm**2')
+
+    # TODO check example data if theses are really pure substances and normalize material properties in results section
+    electrode_material = SubSection(section_def=PubChemPureSubstanceSectionCustom)
+
+    gasket_material = SubSection(section_def=PubChemPureSubstanceSectionCustom)
+
+    gasket_thickness = Quantity(type=np.dtype(np.float64), unit='mm')
+
+
+class ElectrolyserProperties(CompositeSystem):
+    cell_name = Quantity(type=str, shape=[], a_eln=dict(component='StringEditQuantity'))
+
+    test_rigg = Quantity(
+        type=str,
+        shape=[],
+    )
+
+    membrane = Quantity(
+        type=str,
+        shape=[],
+    )
+
+    torque = Quantity(type=np.dtype(np.float64), unit='newton * meter')
+
+    anode = SubSection(section_def=NESDElectrode)
+
+    cathode = SubSection(section_def=NESDElectrode)
+
 
 class ElectrolyserPerformanceEvaluation(BaseMeasurement, PlotSection):
+    # TODO add user name here?
 
     data_file = Quantity(
         type=str,
         a_eln=dict(component='FileEditQuantity'),
-        a_browser=dict(adaptor='RawFileAdaptor'))
+        a_browser=dict(adaptor='RawFileAdaptor'),
+    )
 
-    time = Quantity(
-        type=np.dtype(np.float64),
-        shape=['*'],
-        unit='second')
+    time = Quantity(type=np.dtype(np.float64), shape=['*'], unit='second')
 
-    h2_flow = Quantity(
-        type=np.dtype(np.float64),
-        shape=['*'],
-        unit=('ml/minute'))
+    h2_flow = Quantity(type=np.dtype(np.float64), shape=['*'], unit=('ml/minute'))
 
-    o2_flow = Quantity(
-        type=np.dtype(np.float64),
-        shape=['*'],
-        unit=('ml/minute'))
+    o2_flow = Quantity(type=np.dtype(np.float64), shape=['*'], unit=('ml/minute'))
 
-    anode_in = Quantity(
-        type=np.dtype(np.float64),
-        shape=['*'],
-        unit=('°C'))
+    anode_in = Quantity(type=np.dtype(np.float64), shape=['*'], unit=('°C'))
 
-    cathode_in = Quantity(
-        type=np.dtype(np.float64),
-        shape=['*'],
-        unit=('°C'))
+    cathode_in = Quantity(type=np.dtype(np.float64), shape=['*'], unit=('°C'))
 
-    anode_out = Quantity(
-        type=np.dtype(np.float64),
-        shape=['*'],
-        unit=('°C'))
+    anode_out = Quantity(type=np.dtype(np.float64), shape=['*'], unit=('°C'))
 
-    cathode_out = Quantity(
-        type=np.dtype(np.float64),
-        shape=['*'],
-        unit=('°C'))
+    cathode_out = Quantity(type=np.dtype(np.float64), shape=['*'], unit=('°C'))
 
-    ambient = Quantity(
-        type=np.dtype(np.float64),
-        shape=['*'],
-        unit=('°C'))
+    ambient = Quantity(type=np.dtype(np.float64), shape=['*'], unit=('°C'))
 
     electrolyser_cell_anode = Quantity(
-        type=np.dtype(np.float64),
-        shape=['*'],
-        unit=('°C'))
+        type=np.dtype(np.float64), shape=['*'], unit=('°C')
+    )
 
     electrolyser_cell_cathode = Quantity(
-        type=np.dtype(np.float64),
-        shape=['*'],
-        unit='°C')
+        type=np.dtype(np.float64), shape=['*'], unit='°C'
+    )
 
-    timestamp = Quantity(
-        type=Datetime,
-        shape=['*'])
+    timestamp = Quantity(type=Datetime, shape=['*'])
+
+    properties = SubSection(section_def=ElectrolyserProperties)
 
     def make_flow_figure(self):
-        fig = go.Figure(data=[go.Scatter(name='H2 Flow', x=self.time, y=self.h2_flow,
-                                         line=dict(color='green'))])
-        fig.add_traces(go.Scatter(name='O2 Flow', x=self.time, y=self.o2_flow,
-                                  yaxis='y2', line=dict(color='red')))
-        fig.update_layout(yaxis=dict(title=f'H2 Flow [{self.h2_flow[0].units}]',
-                                     titlefont=dict(color='green'),
-                                     tickfont=dict(color='green')),
-                          yaxis2=dict(title=f'O2 Flow [{self.o2_flow[0].units}]',
-                                      anchor='x',
-                                      overlaying='y', side='right',
-                                      titlefont=dict(color='red'),
-                                      tickfont=dict(color='red')))
-        fig.update_layout(title_text='H2 and O2 Flow over Time', showlegend=True, xaxis={'fixedrange': False})
+        fig = go.Figure(
+            data=[
+                go.Scatter(
+                    name='H2 Flow',
+                    x=self.time,
+                    y=self.h2_flow,
+                    line=dict(color='green'),
+                )
+            ]
+        )
+        fig.add_traces(
+            go.Scatter(
+                name='O2 Flow',
+                x=self.time,
+                y=self.o2_flow,
+                yaxis='y2',
+                line=dict(color='red'),
+            )
+        )
+        fig.update_layout(
+            yaxis=dict(
+                title=f'H2 Flow [{self.h2_flow[0].units}]',
+                titlefont=dict(color='green'),
+                tickfont=dict(color='green'),
+            ),
+            yaxis2=dict(
+                title=f'O2 Flow [{self.o2_flow[0].units}]',
+                anchor='x',
+                overlaying='y',
+                side='right',
+                titlefont=dict(color='red'),
+                tickfont=dict(color='red'),
+            ),
+        )
+        fig.update_layout(
+            title_text='H2 and O2 Flow over Time',
+            showlegend=True,
+            xaxis={'fixedrange': False},
+        )
         return fig
 
     def normalize(self, archive, logger):
-        fig1 = self.make_flow_figure()
-        self.figures = [PlotlyFigure(label='H2 O2 Flow Figure', figure=fig1.to_plotly_json()),]
+        if self.h2_flow is not None:
+            fig1 = self.make_flow_figure()
+            self.figures = [
+                PlotlyFigure(label='H2 O2 Flow Figure', figure=fig1.to_plotly_json()),
+            ]
         super(ElectrolyserPerformanceEvaluation, self).normalize(archive, logger)
--- src/baseclasses/helper/archive_builder/labview_archive.py
+++ src/baseclasses/helper/archive_builder/labview_archive.py
@@ -1,46 +1,124 @@
-import numpy as np
 from datetime import datetime
 
+import numpy as np
 from nomad.units import ureg
+
+from baseclasses import PubChemPureSubstanceSectionCustom
+from baseclasses.chemical_energy.electrolyser_performance import (
+    ElectrolyserProperties,
+    NESDElectrode,
+)
+
+
+def get_pint_from_string(magnitude_string, unit):
+    try:
+        magnitude = np.float64(magnitude_string)
+    except:
+        print(f'Cannot convert {magnitude_string} to pint magnitude.')
+        magnitude = np.float64(0)
+    return magnitude * ureg(unit)
 
 
 def get_electrolyser_properties(metadata):
-    # TODO decide if we need that for e.g. ids or other properties
-    #properties = ElectrolyserProperties()
-    #properties.comments = metadata.get('Comments')
-    name = metadata.get('name')
-    comments = metadata.get('Comments')
-    torque = metadata.get('Torque_Nm')
-    user_name = metadata.get('User_Name')
-    cell_name = metadata.get('Cell_Name')
-    test_rig = metadata.get('Test_Rig')
-    membrane = metadata.get('Membrane')
-    electrolyte_anode = metadata.get('Electrolyte_Anode')
-    catalyst_anode = metadata.get('Catalyst_Anode')
-    area_anode = metadata.get('Electrode_Area_sqmm_Anode')
-    gasket_material_anode = metadata.get('Gasket_Material_Anode')
-    gasket_thickness_anode = metadata.get('Gasket_Thickness_mm_Anode')
-    electrode_material_anode = metadata.get('Electrode_Material_Anode')
-    electrolyte_cathode = metadata.get('Electrolyte_Cathode')
-    catalyst_cathode = metadata.get('Catalyst_Cathode')
-    gasket_material_cathode = metadata.get('Gasket_Material_Cathode')
-    gasket_thickness_cathode = metadata.get('Gasket_Thickness_sqmm_Cathode')
-    area_cathode = metadata.get('Electrode_Area_sqmm_Cathode')
-    electrode_material_cathode = metadata.get('Electrode_Material_Cathode')
-    #return properties
+    properties = ElectrolyserProperties()
+    torque_magnitude = metadata.get('Torque_Nm')
+    properties.torque = get_pint_from_string(torque_magnitude, 'newton * meter')
+    properties.cell_name = metadata.get('Cell_Name')
+    properties.test_rig = metadata.get('Test_Rig')
+    properties.membrane = metadata.get('Membrane')
+
+    anode = NESDElectrode()
+    anode.electrolyte = metadata.get('Electrolyte_Anode')
+    anode.catalyst = metadata.get('Catalyst_Anode')
+    area_anode_magnitude = metadata.get('Electrode_Area_sqmm_Anode')
+    anode.electrode_area = get_pint_from_string(area_anode_magnitude, 'mm ** 2')
+    anode.gasket_material = PubChemPureSubstanceSectionCustom(
+        name=metadata.get('Gasket_Material_Anode'), load_data=False
+    )
+    gasket_thickness_anode_magnitude = metadata.get('Gasket_Thickness_mm_Anode')
+    anode.gasket_thickness = get_pint_from_string(
+        gasket_thickness_anode_magnitude, 'mm'
+    )
+    anode.electrode_material = PubChemPureSubstanceSectionCustom(
+        name=metadata.get('Electrode_Material_Anode'), load_data=False
+    )
+
+    cathode = NESDElectrode()
+    cathode.electrolyte = metadata.get('Electrolyte_Cathode')
+    cathode.catalyst = metadata.get('Catalyst_Cathode')
+    cathode.gasket_material = PubChemPureSubstanceSectionCustom(
+        name=metadata.get('Gasket_Material_Cathode'), load_data=False
+    )
+    gasket_thickness_cathode_magnitude = metadata.get('Gasket_Thickness_mm_Cathode')
+    cathode.gasket_thickness = get_pint_from_string(
+        gasket_thickness_cathode_magnitude, 'mm'
+    )
+    area_cathode_magnitude = metadata.get('Electrode_Area_sqmm_Cathode')
+    cathode.electrode_area = get_pint_from_string(area_cathode_magnitude, 'mm ** 2')
+    cathode.electrode_material = PubChemPureSubstanceSectionCustom(
+        name=metadata.get('Electrode_Material_Cathode'), load_data=False
+    )
+
+    properties.anode = anode
+    properties.cathode = cathode
+    return properties
 
 
 def get_tdms_archive(data, entry_class):
     # TODO units like mln/minute (is mln normliter? then mln/minute might be slpm or standard liter per minute?)
-    entry_class.time = np.array(data['READ_0_Time']) * ureg('s') if 'READ_0_Time' in data.columns else None
-    entry_class.h2_flow = np.array(data['READ_H2_Flow']) * ureg('ml/minute') if 'READ_H2_Flow' in data.columns else None
-    entry_class.o2_flow = np.array(data['READ_O2_Flow']) * ureg('ml/minute') if 'READ_O2_Flow' in data.columns else None
-    entry_class.anode_in = np.array(data['READ_RTD0_A-in']) * ureg('°C') if 'READ_RTD0_A-in' in data.columns else None
-    entry_class.cathode_in = np.array(data['READ_RTD1_C-in']) * ureg('°C') if 'READ_RTD1_C-in' in data.columns else None
-    entry_class.anode_out = np.array(data['READ_RTD2_A-out']) * ureg('°C') if 'READ_RTD2_A-out' in data.columns else None
-    entry_class.cathode_out = np.array(data['READ_RTD3_C-out']) * ureg('°C') if 'READ_RTD3_C-out' in data.columns else None
-    entry_class.ambient = np.array(data['READ_RTD4_amb']) * ureg('°C') if 'READ_RTD4_amb' in data.columns else None
-    entry_class.electrolyser_cell_anode = np.array(data['READ_RTD5_EC-A']) * ureg('°C') if 'READ_RTD5_EC-A' in data.columns else None
-    entry_class.electrolyser_cell_cathode = np.array(data['READ_RTD6_EC-C']) * ureg('°C') if 'READ_RTD6_EC-C' in data.columns else None
-    timestamp_array = np.array(data['READ_Timestamp']) if 'READ_Timestamp' in data.columns else None
-    entry_class.timestamp = [datetime.fromtimestamp(timestamp) for timestamp in timestamp_array]
+    entry_class.time = (
+        np.array(data['READ_0_Time']) * ureg('s')
+        if 'READ_0_Time' in data.columns
+        else None
+    )
+    entry_class.h2_flow = (
+        np.array(data['READ_H2_Flow']) * ureg('ml/minute')
+        if 'READ_H2_Flow' in data.columns
+        else None
+    )
+    entry_class.o2_flow = (
+        np.array(data['READ_O2_Flow']) * ureg('ml/minute')
+        if 'READ_O2_Flow' in data.columns
+        else None
+    )
+    entry_class.anode_in = (
+        np.array(data['READ_RTD0_A-in']) * ureg('°C')
+        if 'READ_RTD0_A-in' in data.columns
+        else None
+    )
+    entry_class.cathode_in = (
+        np.array(data['READ_RTD1_C-in']) * ureg('°C')
+        if 'READ_RTD1_C-in' in data.columns
+        else None
+    )
+    entry_class.anode_out = (
+        np.array(data['READ_RTD2_A-out']) * ureg('°C')
+        if 'READ_RTD2_A-out' in data.columns
+        else None
+    )
+    entry_class.cathode_out = (
+        np.array(data['READ_RTD3_C-out']) * ureg('°C')
+        if 'READ_RTD3_C-out' in data.columns
+        else None
+    )
+    entry_class.ambient = (
+        np.array(data['READ_RTD4_amb']) * ureg('°C')
+        if 'READ_RTD4_amb' in data.columns
+        else None
+    )
+    entry_class.electrolyser_cell_anode = (
+        np.array(data['READ_RTD5_EC-A']) * ureg('°C')
+        if 'READ_RTD5_EC-A' in data.columns
+        else None
+    )
+    entry_class.electrolyser_cell_cathode = (
+        np.array(data['READ_RTD6_EC-C']) * ureg('°C')
+        if 'READ_RTD6_EC-C' in data.columns
+        else None
+    )
+    timestamp_array = (
+        np.array(data['READ_Timestamp']) if 'READ_Timestamp' in data.columns else None
+    )
+    entry_class.timestamp = [
+        datetime.fromtimestamp(timestamp) for timestamp in timestamp_array
+    ]
