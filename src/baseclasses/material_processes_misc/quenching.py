moved mapping to baseclasses and fixed units for new quantities

src/baseclasses/material_processes_misc/quenching.py
@@ -355,10 +355,10 @@ class AirKnifeGasQuenching(GasQuenching):
 
     heat_transfer_coefficient = Quantity(
         type=np.dtype(np.float64),
-        unit=('W m^-2 K^-1'), # CHECK THE UNITS!!!
+        unit=('W/(K*m**2)'),
         a_eln=dict(
             component='NumberEditQuantity',
-            defaultDisplayUnit='W m^-2 K^-1',
+            defaultDisplayUnit='W/(K*m**2)',
             props=dict(minValue=0),
         ),
     )

src/baseclasses/wet_chemical_deposition/slot_die_coating.py
@@ -131,14 +131,13 @@ class SlotDieCoatingProperties(ArchiveSection):
     coated_area = Quantity(
         links=[],
         type=np.dtype(np.float64),
-        unit=('mm²'),
+        unit=('mm**2'),
         a_eln=dict(
             component='NumberEditQuantity',
-            defaultDisplayUnit='mm²',
+            defaultDisplayUnit='mm**2',
             props=dict(minValue=0),
         ),
     )
-
 
 
 class SlotDieCoating(WetChemicalDeposition):