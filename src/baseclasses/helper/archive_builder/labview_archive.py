efb80577ea4d864858e7307d961daa2f99f754da
carla-terboven <carla.terboven@helmholtz-berlin.de>
2025-01-27 17:14:45 +0100

    Refactor and rename

--- src/baseclasses/helper/archive_builder/labview_archive.py
+++ src/baseclasses/helper/archive_builder/labview_archive.py
@@ -1,8 +1,10 @@
+import inspect
 from datetime import datetime
 
 import numpy as np
 from nomad.units import ureg
 
+import baseclasses
 from baseclasses import PubChemPureSubstanceSectionCustom
 from baseclasses.chemical_energy.electrolyser_performance import (
     ElectrolyserProperties,
@@ -11,107 +13,102 @@ from baseclasses.chemical_energy.electrolyser_performance import (
 
 
 def get_pint_from_string(magnitude_string, unit):
+    if magnitude_string is None:
+        return None
     try:
         magnitude = np.float64(magnitude_string)
     except:
         print(f'Cannot convert {magnitude_string} to pint magnitude.')
-        magnitude = np.float64(0)
+        return None
     return magnitude * ureg(unit)
 
 
-def get_electrolyser_properties(metadata):
-    properties = ElectrolyserProperties()
-    torque_magnitude = metadata.get('Torque_Nm')
-    properties.torque = get_pint_from_string(torque_magnitude, 'newton * meter')
-    properties.cell_name = metadata.get('Cell_Name')
-    properties.test_rig = metadata.get('Test_Rig')
-    properties.membrane = metadata.get('Membrane')
-
-    anode = NESDElectrode()
-    anode.electrolyte = metadata.get('Electrolyte_Anode')
-    anode.catalyst = metadata.get('Catalyst_Anode')
-    area_anode_magnitude = metadata.get('Electrode_Area_sqmm_Anode')
-    anode.electrode_area = get_pint_from_string(area_anode_magnitude, 'mm ** 2')
-    anode.gasket_material = PubChemPureSubstanceSectionCustom(
-        name=metadata.get('Gasket_Material_Anode'), load_data=False
+def get_electrode(metadata, electrode_type):
+    electrode = NESDElectrode()
+    electrode.electrolyte = metadata.get(f'Electrolyte_{electrode_type}')
+    electrode.catalyst = metadata.get(f'Catalyst_{electrode_type}')
+    electrode.gasket_material = PubChemPureSubstanceSectionCustom(
+        name=metadata.get(f'Gasket_Material_{electrode_type}'), load_data=False
     )
-    gasket_thickness_anode_magnitude = metadata.get('Gasket_Thickness_mm_Anode')
-    anode.gasket_thickness = get_pint_from_string(
-        gasket_thickness_anode_magnitude, 'mm'
+    gasket_thickness_cathode_magnitude = metadata.get(
+        f'Gasket_Thickness_mm_{electrode_type}'
     )
-    anode.electrode_material = PubChemPureSubstanceSectionCustom(
-        name=metadata.get('Electrode_Material_Anode'), load_data=False
-    )
-
-    cathode = NESDElectrode()
-    cathode.electrolyte = metadata.get('Electrolyte_Cathode')
-    cathode.catalyst = metadata.get('Catalyst_Cathode')
-    cathode.gasket_material = PubChemPureSubstanceSectionCustom(
-        name=metadata.get('Gasket_Material_Cathode'), load_data=False
-    )
-    gasket_thickness_cathode_magnitude = metadata.get('Gasket_Thickness_mm_Cathode')
-    cathode.gasket_thickness = get_pint_from_string(
+    electrode.gasket_thickness = get_pint_from_string(
         gasket_thickness_cathode_magnitude, 'mm'
     )
-    area_cathode_magnitude = metadata.get('Electrode_Area_sqmm_Cathode')
-    cathode.electrode_area = get_pint_from_string(area_cathode_magnitude, 'mm ** 2')
-    cathode.electrode_material = PubChemPureSubstanceSectionCustom(
-        name=metadata.get('Electrode_Material_Cathode'), load_data=False
+    area_cathode_magnitude = metadata.get(f'Electrode_Area_sqmm_{electrode_type}')
+    electrode.electrode_area = get_pint_from_string(area_cathode_magnitude, 'mm ** 2')
+    electrode.electrode_material = PubChemPureSubstanceSectionCustom(
+        name=metadata.get(f'Electrode_Material_{electrode_type}'), load_data=False
     )
-
-    properties.anode = anode
-    properties.cathode = cathode
-    return properties
+    return electrode
 
 
-def get_tdms_archive(data, entry_class):
+def get_electrolyser_properties(metadata, entry_object):
+    assert (
+        baseclasses.chemical_energy.electrolyser_performance.ElectrolyserProperties
+        in inspect.getmro(type(entry_object))
+    )
+    torque_magnitude = metadata.get('Torque_Nm')
+    entry_object.torque = get_pint_from_string(torque_magnitude, 'newton * meter')
+    entry_object.cell_name = metadata.get('Cell_Name')
+    entry_object.test_rigg = metadata.get('Test_Rig')
+    entry_object.membrane = metadata.get('Membrane')
+
+    entry_object.anode = get_electrode(metadata, 'Anode')
+    entry_object.cathode = get_electrode(metadata, 'Cathode')
+
+    return entry_object
+
+
+def get_tdms_archive(data, entry_object):
     # TODO units like mln/minute (is mln normliter? then mln/minute might be slpm or standard liter per minute?)
-    entry_class.time = (
+    entry_object.time = (
         np.array(data['READ_0_Time']) * ureg('s')
         if 'READ_0_Time' in data.columns
         else None
     )
-    entry_class.h2_flow = (
+    entry_object.h2_flow = (
         np.array(data['READ_H2_Flow']) * ureg('ml/minute')
         if 'READ_H2_Flow' in data.columns
         else None
     )
-    entry_class.o2_flow = (
+    entry_object.o2_flow = (
         np.array(data['READ_O2_Flow']) * ureg('ml/minute')
         if 'READ_O2_Flow' in data.columns
         else None
     )
-    entry_class.anode_in = (
+    entry_object.anode_in = (
         np.array(data['READ_RTD0_A-in']) * ureg('°C')
         if 'READ_RTD0_A-in' in data.columns
         else None
     )
-    entry_class.cathode_in = (
+    entry_object.cathode_in = (
         np.array(data['READ_RTD1_C-in']) * ureg('°C')
         if 'READ_RTD1_C-in' in data.columns
         else None
     )
-    entry_class.anode_out = (
+    entry_object.anode_out = (
         np.array(data['READ_RTD2_A-out']) * ureg('°C')
         if 'READ_RTD2_A-out' in data.columns
         else None
     )
-    entry_class.cathode_out = (
+    entry_object.cathode_out = (
         np.array(data['READ_RTD3_C-out']) * ureg('°C')
         if 'READ_RTD3_C-out' in data.columns
         else None
     )
-    entry_class.ambient = (
+    entry_object.ambient = (
         np.array(data['READ_RTD4_amb']) * ureg('°C')
         if 'READ_RTD4_amb' in data.columns
         else None
     )
-    entry_class.electrolyser_cell_anode = (
+    entry_object.electrolyser_cell_anode = (
         np.array(data['READ_RTD5_EC-A']) * ureg('°C')
         if 'READ_RTD5_EC-A' in data.columns
         else None
     )
-    entry_class.electrolyser_cell_cathode = (
+    entry_object.electrolyser_cell_cathode = (
         np.array(data['READ_RTD6_EC-C']) * ureg('°C')
         if 'READ_RTD6_EC-C' in data.columns
         else None
@@ -119,6 +116,6 @@ def get_tdms_archive(data, entry_class):
     timestamp_array = (
         np.array(data['READ_Timestamp']) if 'READ_Timestamp' in data.columns else None
     )
-    entry_class.timestamp = [
+    entry_object.timestamp = [
         datetime.fromtimestamp(timestamp) for timestamp in timestamp_array
     ]
