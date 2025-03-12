fixed some units and types

src/baseclasses/vapour_based_deposition/atomic_layer_deposition.py
@@ -123,10 +123,10 @@ class ALDMaterial(ArchiveSection):
 
     pulse_flow_rate = Quantity(
         type=np.dtype(np.float64),
-        unit=('ccm'),
+        unit=('cm**3/minute'),
         a_eln=dict(
             component='NumberEditQuantity',
-            defaultDisplayUnit='ccm',
+            defaultDisplayUnit='cm**3/minute',
             props=dict(minValue=0),
         ),
     )
@@ -143,10 +143,10 @@ class ALDMaterial(ArchiveSection):
 
     purge_flow_rate = Quantity(
         type=np.dtype(np.float64),
-        unit=('ccm'),
+        unit=('cm**3/minute'),
         a_eln=dict(
             component='NumberEditQuantity',
-            defaultDisplayUnit='ccm',
+            defaultDisplayUnit='cm**3/minute',
             props=dict(minValue=0),
         ),
     )

src/baseclasses/wet_chemical_deposition/inkjet_printing.py
@@ -159,12 +159,12 @@ class NotionNozzleVoltageProfile(NozzleVoltageProfile):
     )
 
     grey_level_0_pulse_a = Quantity(
-        type=np.dtype(np.bool),
+        type=bool,
         a_eln=dict(component='BoolEditQuantity'),
     )
 
     grey_level_1_pulse_a = Quantity(
-        type=np.dtype(np.bool),
+        type=bool,
         a_eln=dict(component='BoolEditQuantity'),
     )
     # Add the rest of the parameters here