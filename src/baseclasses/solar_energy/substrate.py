2d7bbc0d06beea5025fb5581d29c75af395834cd
Dimitra Gkogkou <a5263@nb-hp1ds64.basisit.de>
2025-04-30 19:22:43 +0200

    reverted to substrate props repeats=False because of parsing error

--- src/baseclasses/solar_energy/substrate.py
+++ src/baseclasses/solar_energy/substrate.py
@@ -76,7 +76,7 @@ class Substrate(Entity):
         ),
     )
 
-    substrate_properties = SubSection(section_def=LayerProperties, repeats=True)
+    substrate_properties = SubSection(section_def=LayerProperties)
         
     # back_contact = Quantity(
     #     type=str,
