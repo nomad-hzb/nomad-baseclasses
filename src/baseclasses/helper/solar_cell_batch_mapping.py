b102347171cb0623d7a85131ee59e4dc3914006f
Dimitra Gkogkou <a5263@nb-hp1ds64.basisit.de>
2025-05-26 17:57:00 +0200

    reverted datetime tracking for the time being
        pick ca9e2be first lint

--- src/baseclasses/helper/solar_cell_batch_mapping.py
+++ src/baseclasses/helper/solar_cell_batch_mapping.py
@@ -86,11 +86,10 @@ def get_value(data, key, default=None, number=True, unit=None):
         raise e
 
 
-def map_basic_sample(data, substrate_name, upload_id, datetime, sample_class):
+def map_basic_sample(data, substrate_name, upload_id, sample_class):
     archive = sample_class(
         name=data['Nomad ID'],
         lab_id=data['Nomad ID'],
-        datetime=datetime,
         substrate=get_reference(upload_id, substrate_name) if substrate_name else None,
         description=get_value(data, 'Variation', None, False),
         number_of_junctions=get_value(data, 'Number of junctions', None),
@@ -98,11 +97,10 @@ def map_basic_sample(data, substrate_name, upload_id, datetime, sample_class):
     return (data['Nomad ID'], archive)
 
 
-def map_batch(batch_ids, batch_id, upload_id, datetime, batch_class):
+def map_batch(batch_ids, batch_id, upload_id, batch_class):
     archive = batch_class(
         name=batch_id,
         lab_id=batch_id,
-        datetime=datetime,
         entities=[
             CompositeSystemReference(
                 reference=get_reference(upload_id, f'{lab_id}.archive.json'),
