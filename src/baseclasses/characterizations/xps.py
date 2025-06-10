0244e68957d1509caed38ecf0fd92443e806cb7a
Michael Götte <36420750+RoteKekse@users.noreply.github.com>
2025-06-10 22:01:13 +0200

    added xps schema for prodigy (#92)

--- src/baseclasses/characterizations/xps.py
+++ src/baseclasses/characterizations/xps.py
@@ -16,9 +16,65 @@
 # limitations under the License.
 #
 
-from nomad.metainfo import Quantity, Section
+import numpy as np
+from nomad.datamodel.data import ArchiveSection
+from nomad.metainfo import Quantity, Section, SubSection
 
 from .. import BaseMeasurement, LibraryMeasurement
+
+
+class XPSSpecsLabProdigyAnalyzerParameters(ArchiveSection):
+    polar_angle = Quantity(type=np.dtype(np.float64), unit='degree')
+    azimuth_angle = Quantity(type=np.dtype(np.float64), unit='degree')
+    rotation_angle = Quantity(type=np.dtype(np.float64), unit='degree')
+    coil_current = Quantity(type=np.dtype(np.float64), unit='A')
+    bias_voltage_ions = Quantity(type=np.dtype(np.float64), unit='V')
+    bias_voltage_electrons = Quantity(type=np.dtype(np.float64), unit='V')
+    detector_voltage = Quantity(type=np.dtype(np.float64), unit='V')
+    work_function = Quantity(type=np.dtype(np.float64))
+    focus_displacement = Quantity(type=np.dtype(np.float64))
+    l1 = Quantity(type=np.dtype(np.float64))
+
+
+class XPSSpecsLabProdigySourceParameters(ArchiveSection):
+    polar_angle = Quantity(type=np.dtype(np.float64), unit='degree')
+    azimuth_angle = Quantity(type=np.dtype(np.float64), unit='degree')
+    excitation_energy = Quantity(type=np.dtype(np.float64), unit='eV')
+    device_state = Quantity(type=str)
+    preset_name = Quantity(type=str)
+    anode = Quantity(type=str)
+    anode_voltage = Quantity(type=np.dtype(np.float64))
+    anode_current = Quantity(type=np.dtype(np.float64))
+    filament_current = Quantity(type=np.dtype(np.float64))
+    filament_voltage = Quantity(type=np.dtype(np.float64))
+    power = Quantity(type=np.dtype(np.float64))
+    emission = Quantity(type=np.dtype(np.float64))
+    arcs = Quantity(type=np.dtype(np.float64))
+    lens_voltage = Quantity(type=np.dtype(np.float64))
+    lens_current = Quantity(type=np.dtype(np.float64))
+    focused = Quantity(type=str)
+    settings_summary = Quantity(type=str)
+
+
+class XPSSpecsLabProdigySettings(ArchiveSection):
+    region = Quantity(type=str)
+    calibration_file = Quantity(type=str)
+    analyzer_lens_mode = Quantity(type=str)
+    scan_variable = Quantity(type=str)
+    step_size = Quantity(type=np.dtype(np.float64))
+    dwell_time = Quantity(type=np.dtype(np.float64))
+    excitation_energy = Quantity(type=np.dtype(np.float64))
+    kinetic_energy = Quantity(type=np.dtype(np.float64))
+    binding_energy = Quantity(type=np.dtype(np.float64))
+    pass_energy = Quantity(type=np.dtype(np.float64))
+    bias_voltage = Quantity(type=np.dtype(np.float64))
+    detector_voltage = Quantity(type=np.dtype(np.float64))
+    effective_work_function = Quantity(type=np.dtype(np.float64))
+    iris_diameter = Quantity(type=np.dtype(np.float64))
+    analyzer_slit = Quantity(type=str)
+    analyzer_lens_voltage = Quantity(type=str)
+    analyzer_parameters = SubSection(section_def=XPSSpecsLabProdigyAnalyzerParameters)
+    source_parameters = SubSection(section_def=XPSSpecsLabProdigySourceParameters)
 
 
 class XPS(BaseMeasurement):
