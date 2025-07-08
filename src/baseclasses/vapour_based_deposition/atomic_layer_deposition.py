b49651d3904df2f3048fd06adef6cdbcc50bb144
AsaphHall <aimlikefridge@gmail.com>
2025-07-08 13:48:24 +0200

    added flow_rate to pulse_flow_rate in ald

--- src/baseclasses/vapour_based_deposition/atomic_layer_deposition.py
+++ src/baseclasses/vapour_based_deposition/atomic_layer_deposition.py
@@ -122,6 +122,7 @@ class ALDMaterial(ArchiveSection):
     )
 
     pulse_flow_rate = Quantity(
+        links=["http://purl.obolibrary.org/obo/PATO_0001574"],
         type=np.dtype(np.float64),
         unit=('cm**3/minute'),
         a_eln=dict(
