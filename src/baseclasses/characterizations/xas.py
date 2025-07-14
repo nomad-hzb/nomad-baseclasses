640a189d53c3e25a85961443cbaa80704a2f4add
Carla Terboven <158454895+carla-terboven@users.noreply.github.com>
2025-07-14 11:27:33 +0200

    KMC3 Schema and parser (#110)
    * Add kmc3 sdd detector
    
    * Add sdd details
    
    * Remove k00 because it is only duplicate of k0
    
    * Add correct deadtime correction and remove k00

--- src/baseclasses/characterizations/xas.py
+++ src/baseclasses/characterizations/xas.py
@@ -16,8 +16,15 @@
 # limitations under the License.
 #
 
+import json
+
 import numpy as np
-from nomad.metainfo import Quantity
+from nomad.datamodel.data import ArchiveSection
+from nomad.datamodel.metainfo.plot import PlotlyFigure, PlotSection
+from nomad.metainfo import Quantity, SubSection
+from scipy.optimize import curve_fit
+
+from baseclasses.helper.plotly_plots import make_xas_plot
 
 from .. import BaseMeasurement
 
@@ -96,6 +103,112 @@ class XAS(BaseMeasurement):
 
     def normalize(self, archive, logger):
         super().normalize(archive, logger)
+
+
+class SiliconDriftDetector(PlotSection, ArchiveSection):
+    fluo = Quantity(
+        type=np.dtype(np.float64),
+        shape=['*'],
+    )
+    icr = Quantity(
+        type=np.dtype(np.float64),
+        shape=['*'],
+        description='Input Count Rate',
+    )
+    ocr = Quantity(
+        type=np.dtype(np.float64),
+        shape=['*'],
+        description='Output Count Rate',
+    )
+    tlt = Quantity(
+        type=np.dtype(np.float64),
+        shape=['*'],
+        description='Trigger Life Time',
+    )
+    lt = Quantity(
+        type=np.dtype(np.float64),
+        shape=['*'],
+        description='Life Time',
+    )
+    rt = Quantity(
+        type=np.dtype(np.float64),
+        shape=['*'],
+        description='Real Time',
+    )
+    slope = Quantity(
+        type=np.dtype(np.float64),
+        description='slope=A*k for fit of OCR = A * (1 - exp(-k * ICR))',
+    )
+    fluo_dead_time_corrected = Quantity(
+        type=np.dtype(np.float64),
+        shape=['*'],
+        description='fluo_dead_time_corrected = fluo * slope * ICR/OCR',
+    )
+    fluo_tlt = Quantity(
+        type=np.dtype(np.float64),
+        shape=['*'],
+        description='fluo_tlt = fluo_dead_time_corrected / TLT',
+    )
+    fluo_tlt_result = Quantity(
+        type=np.dtype(np.float64),
+        shape=['*'],
+        description='fluo_tlt_result = fluo_tlt / k0',
+    )
+
+    def normalize(self, archive, logger, k0):
+        super().normalize(archive, logger)
+        if self.fluo is not None and self.icr is not None and self.ocr is not None:
+
+            def exp_func(icr, a, k):
+                return a * (1 - np.exp(-k * icr))
+
+            # estimate starting values
+            a0 = self.ocr.max()
+            k0 = 1e-5
+            try:
+                params, _ = curve_fit(
+                    exp_func, self.icr, self.ocr, p0=(a0, k0), maxfev=10000
+                )
+                a_fit, k_fit = params
+            except Exception:
+                a_fit, k_fit = (1, 1)
+
+            self.slope = a_fit * k_fit
+            self.fluo_dead_time_corrected = (
+                self.fluo
+                * self.slope
+                * self.icr
+                / np.where(self.ocr == 0, np.nan, self.ocr)
+            )
+            self.fluo_tlt = self.fluo_dead_time_corrected / self.tlt
+            self.fluo_tlt_result = self.fluo_tlt / k0
+        fig1 = make_xas_plot('OCR/ICR', self.ocr, 'ICR', [self.icr], 'OCR')
+        self.figures = [
+            PlotlyFigure(label='OCR vs ICR Plot', figure=json.loads(fig1.to_json())),
+        ]
+
+
+class XASWithSDD(XAS, PlotSection):
+    sdd_parameters = SubSection(section_def=SiliconDriftDetector, repeats=True)
+
+    def normalize(self, archive, logger):
+        self.method = 'XAS Fluoresence'
+        for detector in self.sdd_parameters:
+            detector.normalize(archive, logger, self.k0)
+        super().normalize(archive, logger)
+        fluo_result_list = [sdd.get('fluo_tlt_result') for sdd in self.sdd_parameters]
+        fig1 = make_xas_plot(
+            'Absorption of Sample (FluoResult/k0)',
+            self.energy,
+            'Energy',
+            fluo_result_list,
+            'Fluo corrected',
+        )
+        self.figures = [
+            PlotlyFigure(
+                label='Sample Absorbance Plot', figure=json.loads(fig1.to_json())
+            ),
+        ]
 
 
 class XASFluorescence(XAS):
--- src/baseclasses/helper/archive_builder/xas_archive.py
+++ src/baseclasses/helper/archive_builder/xas_archive.py
@@ -1,13 +1,29 @@
 from datetime import datetime
 
+from baseclasses.characterizations.xas import SiliconDriftDetector
+
 
 def get_xas_archive(data, dateline, entry_class):
     if dateline is not None:
         datetime_object = datetime.strptime(dateline, '#D\t%a %b %d\t%H:%M:%S\t%Y')
         entry_class.datetime = datetime_object.strftime('%Y-%m-%d %H:%M:%S.%f')
 
-    entry_class.energy = data['#monoE']
-    entry_class.seconds = data['Seconds']
-    entry_class.k0 = data['K0']
-    entry_class.k1 = data['K1']
-    entry_class.k3 = data['K3']
+    entry_class.energy = data.get('#monoE')
+    entry_class.seconds = data.get('Seconds')
+    entry_class.k0 = data.get('K0')
+    entry_class.k1 = data.get('K1')
+    entry_class.k3 = data.get('K3')
+
+    kmc3_data = []
+    for index in range(1, 14):
+        kmc3_data.append(
+            SiliconDriftDetector(
+                fluo=data.get(f'fluo{index}'),
+                icr=data.get(f'icr{index}'),
+                ocr=data.get(f'ocr{index}'),
+                tlt=data.get(f'tlt{index}'),
+                lt=data.get(f'lt{index}'),
+                rt=data.get(f'rt{index}'),
+            )
+        )
+    entry_class.sdd_parameters = kmc3_data
