e9aeda51cd0c6d4d4494f0bd11529b7357ff82f9
Carla Terboven <158454895+carla-terboven@users.noreply.github.com>
2025-07-17 10:36:48 +0200

    KMC3 parser for new files with header (#111)
    * Add kmc3 sdd detector
    
    * Add sdd details
    
    * Remove k00 because it is only duplicate of k0
    
    * Add correct deadtime correction and remove k00
    
    * Add and rename xas sdd quantities and their description
    
    * Add possibility to also read new KMC3 headers
    
    * ruff
    
    * Make reading mor robust for old kmc3 files without header
    
    * Add description to manual shift

--- src/baseclasses/helper/archive_builder/xas_archive.py
+++ src/baseclasses/helper/archive_builder/xas_archive.py
@@ -5,25 +5,59 @@ from baseclasses.characterizations.xas import SiliconDriftDetector
 
 def get_xas_archive(data, dateline, entry_class):
     if dateline is not None:
-        datetime_object = datetime.strptime(dateline, '#D\t%a %b %d\t%H:%M:%S\t%Y')
+        if dateline.startswith('#D'):
+            datetime_object = datetime.strptime(dateline, '#D\t%a %b %d\t%H:%M:%S\t%Y')
+        elif dateline.startswith('# start_time:'):
+            datetime_object = datetime.strptime(
+                dateline, '# start_time: %Y-%m-%d %H:%M:%S.%f'
+            )
+        else:
+            raise ValueError('Unknown Date format')
         entry_class.datetime = datetime_object.strftime('%Y-%m-%d %H:%M:%S.%f')
 
-    entry_class.energy = data.get('#monoE')
-    entry_class.seconds = data.get('Seconds')
-    entry_class.k0 = data.get('K0')
-    entry_class.k1 = data.get('K1')
-    entry_class.k3 = data.get('K3')
+    entry_class.energy = (
+        data['#monoE']
+        if '#monoE' in data.columns
+        else data['mono_eV'] / 1000
+        if 'mono_eV' in data.columns
+        else data['monoE_eV'] / 1000
+        if 'monoE_eV' in data.columns
+        else None
+    )
+    entry_class.seconds = (
+        data['Seconds']
+        if 'Seconds' in data.columns
+        else data['time_ms'] / 1000
+        if 'time_ms' in data.columns
+        else None
+    )
+    entry_class.k0 = (
+        data['K0'] if 'K0' in data.columns
+        else data['I0_A'] if 'I0_A' in data.columns
+        else None
+    )
+    entry_class.k1 = (
+        data['K1'] if 'K1' in data.columns
+        else data['I1_A'] if 'I1_A' in data.columns
+        else None
+    )
+    entry_class.k3 = (
+        data['K3'] if 'K3' in data.columns
+        else data['I2_A'] if 'I2_A' in data.columns
+        else None
+    )
 
     kmc3_data = []
-    for index in range(1, 14):
+
+    for index in range(0, 13):
         kmc3_data.append(
             SiliconDriftDetector(
-                fluo=data.get(f'fluo{index}'),
-                icr=data.get(f'icr{index}'),
-                ocr=data.get(f'ocr{index}'),
-                tlt=data.get(f'tlt{index}'),
-                lt=data.get(f'lt{index}'),
-                rt=data.get(f'rt{index}'),
+                fluo=data.get(f'fluo.{index}'),
+                icr=data.get(f'ICR.{index}'),
+                ocr=data.get(f'OCR.{index}'),
+                tlt=data.get(f'TLT.{index}'),
+                lt=data.get(f'LT.{index}'),
+                rt=data.get(f'RT.{index}'),
             )
         )
     entry_class.sdd_parameters = kmc3_data
