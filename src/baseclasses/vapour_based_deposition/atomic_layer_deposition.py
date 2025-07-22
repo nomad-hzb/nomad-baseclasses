534dc7e85014689ad09e364b1ddb79d2265799f8
Joshua <joshua.damm@kit.edu>
2025-07-22 17:14:45 +0200

    accepted chances for the ccm to cm**3 / min again???

--- src/baseclasses/vapour_based_deposition/atomic_layer_deposition.py
+++ src/baseclasses/vapour_based_deposition/atomic_layer_deposition.py
@@ -123,17 +123,10 @@ class ALDMaterial(ArchiveSection):
 
     pulse_flow_rate = Quantity(
         type=np.dtype(np.float64),
-<<<<<<< HEAD
         unit=('cm**3 / min'),
         a_eln=dict(
             component='NumberEditQuantity',
             defaultDisplayUnit='cm**3 / min',
-=======
-        unit=('cm**3/min'),
-        a_eln=dict(
-            component='NumberEditQuantity',
-            defaultDisplayUnit='cm**3/min',
->>>>>>> fc19f14f486d4e3ed05dffc8cf44b864635fffbf
             props=dict(minValue=0),
         ),
     )
@@ -150,17 +143,10 @@ class ALDMaterial(ArchiveSection):
 
     purge_flow_rate = Quantity(
         type=np.dtype(np.float64),
-<<<<<<< HEAD
         unit=('cm**3 / min'),
         a_eln=dict(
             component='NumberEditQuantity',
             defaultDisplayUnit='cm**3 / min',
-=======
-        unit=('cm**3/min'),
-        a_eln=dict(
-            component='NumberEditQuantity',
-            defaultDisplayUnit='cm**3/min',
->>>>>>> fc19f14f486d4e3ed05dffc8cf44b864635fffbf
             props=dict(minValue=0),
         ),
     )
@@ -204,17 +190,10 @@ class ALDOxidizerReducer(ArchiveSection):
 
     pulse_flow_rate = Quantity(
         type=np.dtype(np.float64),
-<<<<<<< HEAD
         unit=('cm**3 / min'),
         a_eln=dict(
             component='NumberEditQuantity',
             defaultDisplayUnit='cm**3 / min',
-=======
-        unit=('cm**3/min'),
-        a_eln=dict(
-            component='NumberEditQuantity',
-            defaultDisplayUnit='cm**3/min',
->>>>>>> fc19f14f486d4e3ed05dffc8cf44b864635fffbf
             props=dict(minValue=0),
         ),
     )
@@ -231,17 +210,10 @@ class ALDOxidizerReducer(ArchiveSection):
 
     purge_flow_rate = Quantity(
         type=np.dtype(np.float64),
-<<<<<<< HEAD
         unit=('cm**3 / min'),
         a_eln=dict(
             component='NumberEditQuantity',
             defaultDisplayUnit='cm**3 / min',
-=======
-        unit=('cm**3/min'),
-        a_eln=dict(
-            component='NumberEditQuantity',
-            defaultDisplayUnit='cm**3/min',
->>>>>>> fc19f14f486d4e3ed05dffc8cf44b864635fffbf
             props=dict(minValue=0),
         ),
     )
